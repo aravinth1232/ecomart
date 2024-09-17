9401ff1eb93682269ff23d686bc980d7de17b32a
Aravinth-Altrai <aravinth-altrai@outlook.com>
2024-09-17 16:22:02 +0530

    first commit

---
+++ src/app/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Image from 'next/image'
+import logo from "../assets/Ecomart.png"
+
+const Header = () => {
+  return (
+    <header>
+    
+    
+    </header>
+  )
+}
+
+export default Header
