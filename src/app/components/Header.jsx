8d4abc4c4b863ab19f94858a0a2494f5372e10b1
Aravinth-Altrai <aravinth-altrai@outlook.com>
2024-09-17 21:46:58 +0530

    header added

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import Image from 'next/image'
-import logo from "../assets/Ecomart.png"
+// import Image from 'next/image'
+// import logo from "../assets/Ecomart.png"
 
 const Header = () => {
   return (
     <header>
-    
+    <h1>Header</h1>
     
     </header>
   )
